00a63727324d10050119ce860b0723b7c4c2288b
uhakdt <60895762+uhakdt@users.noreply.github.com>
2021-12-14 15:46:16 +0000

    EnvVars updated, installed axios, endpoints: update, delete and create single product

--- auxillary/EnvVar.js
+++ auxillary/EnvVar.js
@@ -1,2 +1,13 @@
-export const PublishKey = process.env.ENVIRONMENT === 'Development' ? process.env.PUBLISHABLE_KEY_DEV : process.env.PUBLISHABLE_KEY_PROD
-export const SecretKey = process.env.ENVIRONMENT === 'Development' ? process.env.SECRET_KEY_DEV : process.env.SECRET_KEY_PROD
+const env = process.env.ENVIRONMENT === 'Development';
+
+// Stripe
+import Stripe from "stripe";
+
+export const PublishKey = env ? process.env.PUBLISHABLE_KEY_DEV : process.env.PUBLISHABLE_KEY_PROD
+
+const SecretKey = env ? process.env.SECRET_KEY_DEV : process.env.SECRET_KEY_PROD
+export const stripe = Stripe(SecretKey, { apiVersion: "2020-08-27" });
+
+
+// Kaientai
+export const KaientaiApiUrl = env ? process.env.KAIENTAI_API_URL_DEV : process.env.KAIENTAI_API_URL_PROD
--- routes/product.js
+++ routes/product.js
@@ -1,31 +1,93 @@
 import express from 'express';
-import Stripe from "stripe";
-import { SecretKey } from "../auxillary/EnvVar.js";
+import { KaientaiApiUrl, stripe } from "../auxillary/EnvVar.js";
+import axios from 'axios';
 
 const router = express.Router();
-const stripe = Stripe(SecretKey, { apiVersion: "2020-08-27" });
+
+// CREATE PRODUCT
+router.post("/api/v1/product", async (req, res) => {
+  try {
+
+    // Create Product using Stripe API
+    await stripe.products.create({
+      name: product.title,
+      metadata: {
+        'supplierID': product.supplierID,
+        'dimensionsCm': product.dimensionsCm.map(String).join(','),
+        'weightGrams': product.weightGrams.toString()
+      }
+    })
+    .then(async (stripeRes) => {
+
+      // Update StripeApi value in DB
+      await axios.put(`${KaientaiApiUrl}/product/stripe`, {
+        id: product.id,
+        stripeID: stripeRes.id
+      })
+      .then(() => {
+        res.status(201).json({
+          status: "Product Created + Updated in DB",
+          data: {
+            product: stripeRes
+          }
+        })
+      })
+      .catch(err => {
+        console.log(err)
+      });
+    })
+    .catch(err => {
+      console.log(err)
+    });
+  } catch (e) {
+    console.log(e.message);
+    res.status(400).json({ 
+      error: e.message 
+    });
+  }
+});
 
 // CREATE PRODUCTS
 router.post("/api/v1/products", async (req, res) => {
   try {
     const listOfProducts = req.body;
+
+    // Create All Products 
     await Promise.all(listOfProducts.map(async (product) => {
-      const productRes = await stripe.products.create({
+
+      // Create Each Product using Stripe API
+      await stripe.products.create({
         name: product.title,
         metadata: {
+          'supplierID': product.supplierID,
           'dimensionsCm': product.dimensionsCm.map(String).join(','),
           'weightGrams': product.weightGrams.toString()
         }
+      })
+      .then(async (resp) => {
+
+        // Update StripeApi value in DB
+        await axios.put(`${KaientaiApiUrl}/product/stripe`, {
+          id: product.id,
+          stripeID: resp.id
+        })
+        .catch(err => {
+          console.log(err)
+        });
+      })
+      .catch(err => {
+        console.log(err)
       });
-    })).then(resp => {
-      console.log(resp)
+    }))
+    .then(() => {
       res.status(201).json({
         status: "Products Created",
         data: {
           products: listOfProducts
         }
       })
-    }).catch(err => {
+    })
+    .catch(err => {
       console.log(err)
       res.status(400).json({
         error: err
@@ -38,5 +100,64 @@ router.post("/api/v1/products", async (req, res) => {
     });
   }
 });
+
+// UPDATE PRODUCT
+router.put("/api/v1/product", async (req, res) => {
+  try {
+    await stripe.products.update(
+    req.body.stripeID,
+    {metadata: {
+      supplierID: req.body.supplierID,
+      dimensionsCm: req.body.dimensionsCm.map(String).join(','),
+      weightGrams: req.body.weightGrams.toString()
+    }})
+    .then(resp => {
+
+      res.status(204).json({
+        status: "Product Updated",
+        data: {
+          product: resp
+        }
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(400).json({
+        error: err
+      })
+    });
+  } catch (e) {
+    console.log(e.message);
+    res.status(400).json({ 
+      error: e.message 
+    });
+  }
+})
+
+// DELETE PRODUCT
+router.delete("/api/v1/product", async (req, res) => {
+  try {
+    await stripe.products.del(
+      req.body.stripeID
+    )
+    .then(resp => {
+      res.status(202).json({
+        status: "Product Deleted",
+        data: resp
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(400).json({
+        error: err
+      })
+    });
+  } catch (e) {
+    console.log(e.message);
+    res.status(400).json({
+      error: e.message
+    })
+  }
+})
 
 export default router;
